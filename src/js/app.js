795040d1f4001904c99c3536b06fd30c30108ecf
Sergio Lo Porto <51946947+sergioloporto@users.noreply.github.com>
2019-11-10 18:23:00 +0100

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    //Set up the variables
+    let scores, roundScore, activePlayer;
+
+    //Start the players'scores from zero
+    scores = [0, 0];
+    roundScore = 0; //0 is the Active Player. 1 is the other player.
+
+
+});
