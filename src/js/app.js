262b1a60da4868b949b7530d3ef8ee7996780493
Sergio Lo Porto <51946947+sergioloporto@users.noreply.github.com>
2019-11-10 21:02:09 +0100

    first working version

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,16 @@
 window.addEventListener('DOMContentLoaded', (event) => {
 
     //Set up the variables
-    let scores, roundScore, activePlayer, middleDice, score0, score1, current0, current1;
+    let scores, roundScore, activePlayer, middleDice, score0, score1, current0, current1, btnRoll, btnHold;
 
     //Start the players'scores from zero
     scores = [0, 0]; //starting scores for all the players are set to zero
     roundScore = 0; //0 is the Active Player. 1 is the other player.
     activePlayer = 0; //here I set the first player to play
+
+    //The buttons
+    btnRoll = document.querySelector(".btn-roll");
+    btnHold = document.querySelector(".btn-hold");
 
     middleDice = document.querySelector(".dice");
     middleDice.style.display = "none";
@@ -23,10 +27,11 @@ window.addEventListener('DOMContentLoaded', (event) => {
     current0.textContent = "0";
     current1.textContent = "0";
 
+
     /////////////////////////////
     //Adding the eventListener to the Roll Dice button
     /////////////////////////////
-    document.querySelector(".btn-roll").addEventListener("click", () => {
+    btnRoll.addEventListener("click", () => {
         playerPanel0 = document.querySelector(".player-0-panel");
         playerPanel1 = document.querySelector(".player-1-panel");
 
@@ -41,25 +46,49 @@ window.addEventListener('DOMContentLoaded', (event) => {
         middleDice.src = `../img/dice-${dice}.png`;
 
         // The logic
-        if (dice !==1) {
+        if (dice !== 1) {
             //If the number is not 1, add the number to the roundScore
             roundScore += dice;
             document.querySelector(`#current-${activePlayer}`).textContent = roundScore;
+            document.querySelector(`.player-${activePlayer}-panel`).classList.add("winner");
+            document.querySelector(`.player-${activePlayer}-panel`).classList.add("active");
         } else {
-            //If the number is one, Pass to the other player
-            activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-            //Reset the round score for everybody
-            roundScore = 0;
-            current0.textContent = "0";
-            current1.textContent = "0";
-
-            //Adapt the style according to the active player
-            playerPanel0.classList.toggle("active");
-            playerPanel1.classList.toggle("active");
-
-            //Hide the dice as soon as it is the new player's turn
-            middleDice.style.display = "none";
+            nextPlayer();
         }
-
     });
+
+    btnHold.addEventListener("click", () => {
+        //add the current(round) score to the total score in the array. ActivePlayer acts as the array Index;
+        scores[activePlayer] += roundScore;
+
+        document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer];
+
+        if (scores[activePlayer] >= 15) {
+            document.querySelector(`#name-${activePlayer}`).textContent = "WINNER";
+            middleDice.style.display = "none";
+
+        } else {
+            nextPlayer();
+        }
+    });
+
+
+    //Setting up the function to switch player. It will be used in the Roll button and in the Hold button.
+    const nextPlayer = () =>     {
+        //If the number is one, Pass to the other player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        //Reset the round score for everybody
+        roundScore = 0;
+        current0.textContent = "0";
+        current1.textContent = "0";
+
+        //Adapt the style according to the active player
+        playerPanel0.classList.toggle("active");
+        playerPanel1.classList.toggle("active");
+
+        //Hide the dice as soon as it is the new player's turn
+        middleDice.style.display = "none";
+    };
+
+
 });
